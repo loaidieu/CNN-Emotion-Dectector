1cd74a1e3c50c8d6462d3acb2aea2b6d10400797
Anh Quan Truong <quanat1x4@gmail.com>
2024-03-30 02:30:52 -0400

    added rotate and brightness adjustment functions

--- data_processing.py
+++ data_processing.py
@@ -56,14 +56,59 @@ def flip_png(images_folder):
                 # save it
                 flipped_image.save(filename_wo_extension + '_flipped.png')
 
-# check and stop if there are any flipped images in the folder (post data augmentation)
-def check_flipped(images_folder):
+
+def rotate_png(images_folder, rotation_angle=30):
     for emotion_folder in sorted(os.listdir(images_folder)):
         # create path for each emotion folder
         emotion_folder_path = os.path.join(images_folder, emotion_folder)
 
         for filename in sorted(os.listdir(emotion_folder_path)):
-            if '_flipped' in filename:
+
+            if filename.endswith('.png'):
+
+                image_path = os.path.join(emotion_folder_path, filename)
+                img        = Image.open(image_path)
+
+                # define the rotation transformation
+                rotation_transform = transforms.RandomRotation(degrees=(-rotation_angle, rotation_angle))
+
+                # apply rotation to the image
+                rotated_image = rotation_transform(img)
+
+                # extract the filename (without extension) from the original image path
+                filename_wo_extension = os.path.splitext(image_path)[0]
+
+                # save it
+                rotated_image.save(filename_wo_extension + '_rotated.png')
+
+def adjust_light_png(images_folder):
+    for emotion_folder in sorted(os.listdir(images_folder)):
+        # create path for each emotion folder
+        emotion_folder_path = os.path.join(images_folder, emotion_folder)
+
+        for filename in sorted(os.listdir(emotion_folder_path)):
+
+            if filename.endswith('.png'):
+
+                image_path = os.path.join(emotion_folder_path, filename)
+                img        = Image.open(image_path)
+
+                light_adjusted_image = transforms.functional.adjust_brightness(img, 0.5)
+
+                # extract the filename (without extension) from the original image path
+                filename_wo_extension = os.path.splitext(image_path)[0]
+
+                # save it
+                light_adjusted_image.save(filename_wo_extension + '_light_adjusted.png')
+
+# check and stop if there are already modified images in the folder (post data augmentation)
+def check_modified(images_folder):
+    for emotion_folder in sorted(os.listdir(images_folder)):
+        # create path for each emotion folder
+        emotion_folder_path = os.path.join(images_folder, emotion_folder)
+
+        for filename in sorted(os.listdir(emotion_folder_path)):
+            if '_flipped' in filename or '_rotated' in filename or '_light_adjusted' in filename:
                 return True
 
         return False
