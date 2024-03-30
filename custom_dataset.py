9e6f25a54567b84968a813c88c4089f4a15a5f97
Anh Quan Truong <quanat1x4@gmail.com>
2024-03-30 16:10:02 -0400

    made a class to put X and y into a Dataset (for dataloader)

---
+++ custom_dataset.py
@@ -0,0 +1,13 @@
+from tools_lib import *
+
+class CustomDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    # method from Dataset class
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index):
+        return (self.X[index], self.y[index])
