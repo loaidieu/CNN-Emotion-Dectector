5a4614a527ad4115c344ac637acde7274f972675
quanatr <quanat1x4@gmail.com>
2024-04-13 09:10:40 -0400

    updated main file of a3

--- a3.py
+++ a3.py
@@ -17,6 +17,8 @@ from utils.tools_lib import DataLoader, pd
 from models.main_model import MainCnn
 from training_and_testing.train_loop import train_loop
 from training_and_testing.predict import predict
+from ten_fold_cv import ten_fold_cv
+from bias_detector import bias_detector
 
 ####################################################################################################################################################################
 # Data Processing
@@ -94,21 +96,23 @@ wd = 0.001
 
 # initialize models
 main_model  = MainCnn(
-    cnn_layer1_kernels=16,
+    cnn_layer1_kernels=32,
     cnn_layer1_kernel_size=3,
     cnn_layer1_padding=None,
     cnn_layer1_poolsize=2,
     cnn_layer1_dropout=0.25,
-    cnn_layer2_kernels=32,
+
+    cnn_layer2_kernels=64,
     cnn_layer2_kernel_size=5,
     cnn_layer2_padding=None,
     cnn_layer2_poolsize=2,
     cnn_layer2_dropout=0.25,
-    cnn_layer3_kernels=64,
+
+    cnn_layer3_kernels=128,
     cnn_layer3_kernel_size=7,
     cnn_layer3_padding=None,
     cnn_layer3_poolsize=2,
-    cnn_layer3_dropout=0.1
+    cnn_layer3_dropout=0.25,
 )
 
 # loss function
@@ -118,7 +122,7 @@ loss = torch.nn.CrossEntropyLoss()
 main_optimizer = torch.optim.Adam(main_model.parameters(), lr=lr, weight_decay=wd, betas=(0.9, 0.999))
 
 # number of epochs
-epochs = 50
+epochs = 100
 
 # variables needed for early stopping
 best_val_loss = float('inf')
@@ -129,101 +133,46 @@ stop_epoch    = 1             # keep track of at which epoch early stopping is t
 ####################################################################################################################################################################
 # 10 fold cross validation
 ####################################################################################################################################################################
-if not os.path.exists('models/trained'):
-    os.makedirs('models/trained')
+# if run is set to True, the function will run the 10 fold cross validation
+ten_fold_cv(run=False, 
+            X_trn=X_trn, 
+            y_trn=y_trn, 
+            loss=loss, 
+            lr=lr, wd=wd,
+            epochs=epochs, 
+            patience=patience)
 
-# initialize the kfold object
-kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+####################################################################################################################################################################
+# bias detection
+####################################################################################################################################################################
+# load the model
+model = MainCnn()
+model.load_state_dict(torch.load('models/trained/trained_main_model.pkl'))
 
-# initialize the variables to store the scores
-accuracy_scores = []
-precision_scores_macro = []
-precision_scores_micro = []
-recall_scores_macro = []
-recall_scores_micro = []
-f1_scores_macro = []
-f1_scores_micro = []
+age_bias_df = bias_detector(model, test_images, 'age')
+gender_bias_df = bias_detector(model, test_images, 'gender')
 
-main_train_losses, main_train_accuracies, main_val_losses, main_val_accuracies = train_loop(main_model, main_optimizer, train_loader, val_loader, loss, epochs, patience)
+bias_df = age_bias_df._append(gender_bias_df)
 
-# loop through the 10 folds
-for train_index, val_index in kfold.split(X_trn):
-    # split the data
-    X_trn_fold, X_val_fold = X_trn[train_index], X_trn[val_index]
-    y_trn_fold, y_val_fold = y_trn[train_index], y_trn[val_index]
+# select the rows 'age average' and 'gender average'
+selected_rows = bias_df[(bias_df['Attribute'] == 'age') & (bias_df['Group'] == 'Average') |
+                        (bias_df['Attribute'] == 'gender') & (bias_df['Group'] == 'Average')]
 
-    # Create dataloader objects for train and validation data
-    train_data = CustomDataset(X_trn_fold, y_trn_fold)
-    val_data = CustomDataset(X_val_fold, y_val_fold)
+# calculate the mean of each metric in the selected rows
+avg_metrics = selected_rows[['Accuracy', 'Precision', 'Recall', 'F1']].mean()
 
-    train_loader = DataLoader(dataset=train_data, batch_size=128)
-    val_loader = DataLoader(dataset=val_data, batch_size=128)
-
-    # initialize the model
-    model = MainCnn()
-
-    # initialize the optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd, betas=(0.9, 0.999))
-
-    # train the model
-    train_losses, train_accuracies, val_losses, val_accuracies = train_loop(model, optimizer, train_loader, val_loader, loss, epochs, patience)
-
-    # predict the validation data
-    y_pred = predict(val_loader, model)
-
-    # calculate the accuracy
-    accuracy = accuracy_score(y_val_fold, y_pred)
-    precision_macro = precision_score(y_val_fold, y_pred, average='macro', zero_division=1)
-    precision_micro = precision_score(y_val_fold, y_pred, average='micro', zero_division=1)
-    recall_macro = recall_score(y_val_fold, y_pred, average='macro', zero_division=1)
-    recall_micro = recall_score(y_val_fold, y_pred, average='micro', zero_division=1)
-    f1_macro = f1_score(y_val_fold, y_pred, average='macro', zero_division=1)
-    f1_micro = f1_score(y_val_fold, y_pred, average='micro', zero_division=1)
-
-    # append the scores to the lists
-    accuracy_scores.append(accuracy)
-    precision_scores_macro.append(precision_macro)
-    precision_scores_micro.append(precision_micro)
-    recall_scores_macro.append(recall_macro)
-    recall_scores_micro.append(recall_micro)
-    f1_scores_macro.append(f1_macro)
-    f1_scores_micro.append(f1_micro)
-
-# print the scores
-print('Accuracy:', np.mean(accuracy_scores))
-print('Precision Macro:', np.mean(precision_scores_macro))
-print('Precision Micro:', np.mean(precision_scores_micro))
-print('Recall Macro:', np.mean(recall_scores_macro))
-print('Recall Micro:', np.mean(recall_scores_micro))
-print('F1 Macro:', np.mean(f1_scores_macro))
-print('F1 Micro:', np.mean(f1_scores_micro))
-
-# save the metrics into a file
-metrics = {
-    'Fold': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Average'],
-    'Macro': {
-        'Precision': precision_scores_macro + [np.mean(precision_scores_macro)],
-        'Recall': recall_scores_macro + [np.mean(recall_scores_macro)],
-        'F1': f1_scores_macro + [np.mean(f1_scores_macro)]
-    },
-    'Micro': {
-        'Precision': precision_scores_micro + [np.mean(precision_scores_micro)],
-        'Recall': recall_scores_micro + [np.mean(recall_scores_micro)],
-        'F1': f1_scores_micro + [np.mean(f1_scores_micro)]
-    },
-    'Accuracy': accuracy_scores + [np.mean(accuracy_scores)],
-}
-
-# Create a MultiIndex DataFrame
-metrics_df = pd.DataFrame({
-    ('', 'Fold'): ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Average'],
-    ('Macro', 'Precision'): metrics['Macro']['Precision'],
-    ('Macro', 'Recall'): metrics['Macro']['Recall'],
-    ('Macro', 'F1'): metrics['Macro']['F1'],
-    ('Micro', 'Precision'): metrics['Micro']['Precision'],
-    ('Micro', 'Recall'): metrics['Micro']['Recall'],
-    ('Micro', 'F1'): metrics['Micro']['F1'],
-    ('', 'Accuracy'): metrics['Accuracy']
+# create a new DataFrame with the average metrics
+avg_df = pd.DataFrame({
+    'Attribute': ['Average'],
+    'Group': ['All'],
+    'Accuracy': [avg_metrics['Accuracy']],
+    'Precision': [avg_metrics['Precision']],
+    'Recall': [avg_metrics['Recall']],
+    'F1': [avg_metrics['F1']]
 })
 
-metrics_df.to_csv('results/kfold_cv.csv', index=False)
+# Append the average metrics DataFrame to the original DataFrame
+bias_df = bias_df._append(avg_df, ignore_index=True)
+
+# save the bias table into a file
+bias_df.to_csv(f'results/bias.csv', index=False)
