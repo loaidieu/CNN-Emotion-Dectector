e2974f11157f54dc90b8145f4db1a369334b412b
quanatr <quanat1x4@gmail.com>
2024-04-13 06:48:11 -0400

    updated the main model

--- models/main_model.py
+++ models/main_model.py
@@ -3,40 +3,175 @@ from utils.tools_lib import *
 torch.manual_seed(0) # reproducibility
 
 class MainCnn(torch.nn.Module):
-    def __init__(self):
+    def __init__(
+            self,
+
+            # conv layer 1
+            cnn_layer1_kernels=16,
+            cnn_layer1_kernel_size=3,
+            cnn_layer1_padding=None,
+            cnn_layer1_poolsize=2,
+            cnn_layer1_dropout=0.1,
+
+            # conv layer 2
+            cnn_layer2_kernels=32,
+            cnn_layer2_kernel_size=5,
+            cnn_layer2_padding=None,
+            cnn_layer2_poolsize=2,
+            cnn_layer2_dropout=0.1,
+
+            # conv layer 3
+            cnn_layer3_kernels=64,
+            cnn_layer3_kernel_size=7,
+            cnn_layer3_padding=None,
+            cnn_layer3_poolsize=2,
+            cnn_layer3_dropout=0.1
+            ):
         super(MainCnn, self).__init__()
 
-        # first convolutional layer with 3x3 filter size
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=3//2)
-        self.bn1   = torch.nn.BatchNorm2d(16)                                                         # batch normalization layer
-        self.relu1 = torch.nn.LeakyReLU(negative_slope=0.01)
-        self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        if cnn_layer1_padding is None: cnn_layer1_padding = cnn_layer1_kernel_size // 2
+        if cnn_layer2_padding is None: cnn_layer2_padding = cnn_layer2_kernel_size // 2
+        if cnn_layer3_padding is None: cnn_layer3_padding = cnn_layer3_kernel_size // 2
 
-        # second convolutional layer with 5x5 filter size
-        self.conv2 = torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, padding=5//2)
-        self.bn2   = torch.nn.BatchNorm2d(32)                                                         # batch normalization layer
-        self.relu2 = torch.nn.LeakyReLU(negative_slope=0.01)
-        self.pool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        ##########################################################################################################
+        # CONVOLUTIONAL MODULE
+        ##########################################################################################################
+        self.conv_module = torch.nn.Sequential()
 
-        # third convolutional layer with 7x7 filter size
-        self.conv3 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, padding=7//2)
-        self.bn3   = torch.nn.BatchNorm2d(64)                                                         # batch normalization layer
-        self.relu3 = torch.nn.LeakyReLU(negative_slope=0.01)
-        self.pool3 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        # 1st convolutional layer
+        self.conv_module.add_module(
+            'conv1', 
+            torch.nn.Conv2d(
+                in_channels=1, 
+                out_channels=cnn_layer1_kernels,
+                kernel_size=cnn_layer1_kernel_size, 
+                padding=cnn_layer1_padding)
+        )
 
-        # fully connected layer
-        self.flatten = torch.nn.Flatten()
-        self.linear  = torch.nn.Linear(in_features=64*6*6, out_features=4)                            # since input image size is 48x48
+        self.conv_module.add_module(
+            'bn1',
+            torch.nn.BatchNorm2d(cnn_layer1_kernels)
+        )
 
-        # dropout rate
-        self.dropout = torch.nn.Dropout(p=0.1)
+        self.conv_module.add_module(
+            'relu1',
+            torch.nn.ReLU()
+        )
+
+        self.conv_module.add_module(
+            'pool1',
+            torch.nn.MaxPool2d(
+                kernel_size=cnn_layer1_poolsize, 
+                stride=cnn_layer1_poolsize)
+        )
+
+        self.conv_module.add_module(
+            'dropout1',
+            torch.nn.Dropout(p=cnn_layer1_dropout)
+        )
+
+        # 2nd convolutional layer
+        self.conv_module.add_module(
+            'conv2', 
+            torch.nn.Conv2d(
+                in_channels=cnn_layer1_kernels, 
+                out_channels=cnn_layer2_kernels,
+                kernel_size=cnn_layer2_kernel_size, 
+                padding=cnn_layer2_padding)
+        )
+
+        self.conv_module.add_module(
+            'bn2',
+            torch.nn.BatchNorm2d(cnn_layer2_kernels)
+        )
+
+        self.conv_module.add_module(
+            'relu2',
+            torch.nn.ReLU()
+        )
+
+        self.conv_module.add_module(
+            'pool2',
+            torch.nn.MaxPool2d(
+                kernel_size=cnn_layer2_poolsize, 
+                stride=cnn_layer2_poolsize)
+        )
+
+        self.conv_module.add_module(
+            'dropout2',
+            torch.nn.Dropout(p=cnn_layer2_dropout)
+        )
+
+        # 3rd convolutional layer
+        self.conv_module.add_module(
+            'conv3', 
+            torch.nn.Conv2d(
+                in_channels=cnn_layer2_kernels, 
+                out_channels=cnn_layer3_kernels,
+                kernel_size=cnn_layer3_kernel_size, 
+                padding=cnn_layer3_padding)
+        )
+
+        self.conv_module.add_module(
+            'bn3',
+            torch.nn.BatchNorm2d(cnn_layer3_kernels)
+        )
+
+        self.conv_module.add_module(
+            'relu3',
+            torch.nn.ReLU()
+        )
+
+        self.conv_module.add_module(
+            'pool3',
+            torch.nn.MaxPool2d(
+                kernel_size=cnn_layer3_poolsize, 
+                stride=cnn_layer3_poolsize)
+        )
+
+        self.conv_module.add_module(
+            'dropout3',
+            torch.nn.Dropout(p=cnn_layer3_dropout)
+        )
+
+        ##########################################################################################################
+        # DENSE MODULE
+        ##########################################################################################################
+        # sample input to get the number of features
+        sample_input = torch.randn(1, 1, 48, 48)
+        sample_output = self.conv_module(sample_input)
+        
+        self.dense_module = torch.nn.Sequential()
+
+        # flatten
+        self.dense_module.add_module(
+            'flatten',
+            torch.nn.Flatten()
+        )
+
+        # linear
+        self.dense_module.add_module(
+            'linear',
+            torch.nn.Linear(
+                in_features=self.num_flat_features(sample_output),
+                out_features=4)
+        )
+
+        # softmax
+        self.dense_module.add_module(
+            'softmax',
+            torch.nn.LogSoftmax(dim=1)
+        )
+
+    def num_flat_features(self, x):
+        size = x.size()[1:] # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s  
+        return num_features
 
     def forward(self, x):
         x = x.float()
-        x = self.pool1(self.relu1(self.bn1(self.conv1(x))))
-        x = self.pool2(self.relu2(self.bn2(self.conv2(x))))
-        x = self.pool3(self.relu3(self.bn3(self.conv3(x))))
-        x = self.flatten(x)
-        x = self.dropout(x)
-        x = self.linear(x)
+        x = self.conv_module(x)
+        x = self.dense_module(x)
         return x
