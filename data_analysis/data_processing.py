93b73f2731c5857c5baf29729ab5dab65086ec6f
Anh Quan Truong <quanat1x4@gmail.com>
2024-04-13 05:45:59 -0400

    updated the way info of an image is stored

--- data_analysis/data_processing.py
+++ data_analysis/data_processing.py
@@ -1,39 +1,63 @@
 from utils.tools_lib import *
 
-# images to numpy arrays conversion
-def png_to_numpy(images_folder):
+# images to dictionary conversion
+def png_to_dict(images_folder):
     # create 2 empty lists, one for features and one for labels
-    features = []
-    labels   = []
+    images = {}
 
     for emotion_folder in sorted(os.listdir(images_folder)):
         # create path for each emotion folder
         emotion_folder_path = os.path.join(images_folder, emotion_folder)
 
+        # skip the .DS_Store file
+        if emotion_folder == '.DS_Store': continue
+
+        # loop through all the files in the folder and find metadata csv file
         for filename in sorted(os.listdir(emotion_folder_path)):
 
-            if filename.endswith('.png'):
-                # open the image
-                image_path = os.path.join(emotion_folder_path, filename)
-                img        = Image.open(image_path)
+            # metadata 
+            if filename.endswith('.csv'): 
+                # read the csv file
+                metadata_df = pd.read_csv(os.path.join(emotion_folder_path, filename))
 
-                # convert the image into a numpy array
-                img_array = np.array(img)
+                if 'age' not in metadata_df.columns and 'gender' not in metadata_df.columns:
+                    # convert the file_attributes column to a dictionary
+                    metadata_df['file_attributes'] = metadata_df['file_attributes'].apply(json.loads)
 
-                # flatten the array to size 2304 (48x48)
-                img_array_flat = img_array.flatten()
+                    # make a new column for age
+                    metadata_df['age'] = metadata_df['file_attributes'].apply(lambda x: x['age'])
 
-                # append the flattened array to the features list
-                features.append(img_array_flat)
+                    # make a new column for gender
+                    metadata_df['gender'] = metadata_df['file_attributes'].apply(lambda x: x['gender'])
 
-                # append the label to the labels list
-                labels.append(emotion_folder)
+                for index, row in metadata_df.iterrows():
+                    # get the filename
+                    image_filename = row['filename']
 
-        # lastly convert both lists into numpy arrays
-        features_np = np.array(features)
-        labels_np   = np.array(labels)
+                    # open the image
+                    image_path = os.path.join(emotion_folder_path, image_filename)
+                    img        = Image.open(image_path)
 
-    return features_np, labels_np
+                    # convert the image into a numpy array
+                    img_array = np.array(img)
+
+                    # flatten the array to size 2304 (48x48)
+                    img_array_flat = np.array(img_array.flatten())
+
+                    # get gender
+                    img_gender = metadata_df[metadata_df['filename'] == image_filename]['gender']
+
+                    # get age
+                    img_age = metadata_df[metadata_df['filename'] == image_filename]['age']
+
+                    # add to dictionary (key: filename, value: dictionary with np array, path, age, gender, emotion)
+                    images[f'{emotion_folder}/{image_filename}'] = {'np_array': img_array_flat,
+                                                              'path': image_path,
+                                                              'age': img_age,
+                                                              'gender': img_gender,
+                                                              'emotion': emotion_folder}
+
+    return images
 
 # flipping every image in a folder for data augmentation
 def flip_png(images_folder):
